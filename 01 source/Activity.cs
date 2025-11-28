9bfa47898c14fed4b34c122478b9bcccb0b90bb6
Anna Vognstoft Mikkelsen <anmi9@student.eadania.dk>
2025-11-28 13:59:12 +0100

    Rettelser og oprettelse af ny klasse: Activity

---
+++ 01 source/Activity.cs
@@ -0,0 +1,31 @@
+﻿/// <summary>
+/// Activity-klassen repræsenterer en aktivitet, der indeholder en liste af sessions.
+/// </summary>
+/// <authors names = "Alle"/>
+/// 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.semesterprøve
+{
+    public class Activity 
+    {
+        private List<Session> listOfSession = new List<Session>();
+        public List<Session> ListOfSession
+        {
+            get
+
+            {
+                return listOfSession;
+            }
+            set
+            {
+                listOfSession = value;
+            }
+        }
+
+    }
+}
