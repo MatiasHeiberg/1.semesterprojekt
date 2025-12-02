42db741c1392a211ab5589bb0c8e10de579dc61e
Lasse Agerskov <lasse.agerskov@gmail.com>
2025-12-02 13:01:38 +0100

    Indlæsning fra json filen og valg af hvilken af bruger objekterne til skal sættes i currentuser i state

--- 01 source/Program.cs
+++ 01 source/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace semesterprøve
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+namespace semesterprøve
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Læs Users.json fra fil.
+            string jsonpath = "Users.json";
+            string json = File.ReadAllText(jsonpath);
+
+            //Deserialiser til liste af User objekter med Name (User.Name) IsAdmin (User.IsAdmin)
+            List<User> AllUsers = JsonSerializer.Deserialize<List<User>>(json);
+
+            //Opretter State
+            State state = new State();
+
+            //Vælger hvilken bruger der er "logget ind"
+            //ændre index fra 0..9
+            state.CurrentUser = AllUsers[0];
+
         }
     }
 }
