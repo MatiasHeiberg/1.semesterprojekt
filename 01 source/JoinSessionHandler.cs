425d9c30bdb5f71171a582c45fe3e13c959c9b5b
Lasse Agerskov <lasse.agerskov@gmail.com>
2025-12-02 12:43:49 +0100

    Update JoinSessionHandler.cs

--- 01 source/JoinSessionHandler.cs
+++ 01 source/JoinSessionHandler.cs
@@ -9,36 +9,39 @@ namespace semesterprøve
 {
     //JoinSessionHandler håndtere tilmelding til af den aktualle bruger fra state til
     //en konkret session.
-    
+
     internal class JoinSessionHandler
     {
-        //Den session som brugeren skal tilmeldes.
-        private Session session;
-        //Den bruger som skal tilmeldes til sessionen.
-        private State state;
+
+        private Session session; //Den session som brugeren skal tilmeldes.
+        private State state; //Den bruger som skal tilmeldes til sessionen.
+
 
         //Constructor som modtager den valgte session og nuværende state (bruger).
         //Kalder JoinSession() med det samme.
         public JoinSessionHandler(Session session, State state)
         {
-           
+
             this.session = session;
             this.state = state;
 
             JoinSession();
         }
+
+
+        private User GetCurrentUser
+        {
+            get
+            {
+                return state.CurrentUser;
+            }
+        }
         //Henter currentUser fra GetCurrentUser(som henter den fra state) og sender brugeren videre
         //til session.AddParticipant() som selv kalder Permission.CanJoinSession.
         private void JoinSession()
         {
-            User currentUser = GetCurrentUser();
+            User currentUser = GetCurrentUser;
             session.AddParticipant(currentUser);
-        }
-        private User GetCurrentUser()
-        {
-            User user;
-            user = state.CurrentUser;
-            return user;
         }
     }
 }
