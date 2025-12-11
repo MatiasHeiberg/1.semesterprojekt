c976e0e7b12ab10b1503c433934cfde85ff1f565
Matias <perm_diners7z@icloud.com>
2025-12-11 13:41:42 +0100

    kode kommentar

--- 01 source/IOFile.cs
+++ 01 source/IOFile.cs
@@ -1,43 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-
-namespace semesterprøve
+﻿namespace semesterprøve
 {
+    /// <summary>
+    /// Håndterer I/O til en CSV fil.
+    /// </summary>
+    /// <authors names = "Alle"/>
     public class IOFile
     {
-
-
         public static User ReadCurrentUser()
         {
-            User currentUser = new User();
-            string line = File.ReadAllText("CurrentUser.csv");
-            currentUser = ExtractUser(line);
+            User currentUser = new User(); 
+            string line = File.ReadAllText("CurrentUser.csv"); // Gemmer bruger tekst-objektet som en lang string
+            currentUser = ExtractUser(line); // Gemmer tekst-objektet som et User objekt
             return currentUser;
         }
-
+        /// <summary>
+        /// Opretter et User objekt ud fra et tekst-objekt
+        /// </summary>
         private static User ExtractUser(string line)
         {
-            string[] parts = line.Split(',');
-            string name = parts[0];
-            bool isAdmin = bool.Parse(parts[1]);
+            string[] parts = line.Split(','); // Opdeler alle objektets attributter efter komma delimeter
+            string name = parts[0]; // Gemmer den første substring som navn
+            bool isAdmin = bool.Parse(parts[1]); // Gemmer den anden substring som isAdmin
             return new User(name, isAdmin);
         }
 
         public static void WriteCurrentUser(User currentUser)
         {
             {
-                var line = $"{currentUser.Name},{currentUser.IsAdmin}";
-
-                // Overskriver hele filen
-                File.WriteAllText("CurrentUser.csv", line);
+                string line = $"{currentUser.Name},{currentUser.IsAdmin}";
+                File.WriteAllText("CurrentUser.csv", line); // Overskriver hele filen med den nye bruger
             }
         }
-
         public static List<User> LoadUsers(string csvPath)
         {
             //Læser hele filen på én gang. Resultatet er et array af strings.
--- 01 source/Permission.cs
+++ 01 source/Permission.cs
@@ -2,7 +2,6 @@
 /// Permission-klassen repræsenterer et system til håndtering af brugerrettigheder.
 /// </summary>
 /// <authors names = "Alle"/>
-/// 
 using System;
 
 namespace semesterprøve
